ade9c6360e1811b7abb6c64212f317296f610bec
jonmsawyer <jonmsawyer@gmail.com>
2018-04-05 00:35:05 -0800

    Update

--- maio/views/dashboard.py
+++ maio/views/dashboard.py
@@ -6,5 +6,15 @@ from maio.models import Media
 @login_required
 def dashboard(request):
     cd = {}
-    cd['images'] = list(Media.get_all_images(request))
+    width = 260
+    cd['images'] = Media.get_all_images(request)
+    cd['width'] = width
+    for image in cd['images']:
+        if image.width > image.height:
+            image.margin_left = width - int(image.width)
+            image.margin_top = 0
+        else:
+            image.margin_top = width - int(image.height)
+            image.margin_left = 0
+    raise Exception(image.margin_left, image.margin_top, image.width, image.height)
     return render(request, 'maio/dashboard.html', cd)
